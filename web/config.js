bfc199a7037b8850f2fd3cd6f06c996441cdf079
Mathieu Dupont <mathieu.jean.dupont@gmail.com>
2025-12-31 11:28:47 -0500

    Imhotep

--- web/config.js
+++ web/config.js
@@ -1,7 +1,7 @@
 const config = {
-    nom: "Code - Nom du cours",
-    description: "Site de référence du cours de x",
-    nomUrl: "depinfo-gabarit"
+    nom: "420-SN1 - Programmation en sciences",
+    description: "Site du cours 420-SN1 Programmation en sciences au cégep Édouard-Montpetit.",
+    nomUrl: "420-SN1"
 }
 
 module.exports = config;
